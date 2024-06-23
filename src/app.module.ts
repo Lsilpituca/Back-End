d80318744e0729eb02a6deb576f6f551d8f2654c
Lorenzo Silpituca <lorenzosilpituca@gmail.com>
2024-06-22 21:57:33 -0300

    Conecto la base de datos al proyecto (mongoose)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/tiendaVirtual')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
